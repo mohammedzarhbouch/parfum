3afc211dbbe14b67ada5b90979f7f9b6dd0aad9c
mohammedzarhbouch <mohamada99@hotmail.com>
2024-03-11 13:46:39 +0100

    front-end bijna af
    focus op back-end morgen vergeet niet de video in google drive

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
-window.addEventListener('scroll', function () {
-    let scrollPosition = window.scrollY;
-    let opacity = scrollPosition / 400; // Adjust the division value for the fade effect
+document.addEventListener("DOMContentLoaded", function () {
 
-    // Ensure opacity is within the range [0, 1]
-    opacity = Math.min(1, Math.max(0, opacity));
 
-    document.querySelector('.header-container').style.backgroundColor = `rgba(212, 175, 55, ${opacity})`;
+    window.addEventListener('scroll', function () {
+        let scrollPosition = window.scrollY;
+        let opacity = scrollPosition / 100; // Adjust the division value for the fade effect
+
+        // Ensure opacity is within the range [0, 1]
+        opacity = Math.min(1, Math.max(0, opacity));
+
+        document.querySelector('.header-container').style.backgroundColor = `rgba(212, 175, 55, ${opacity})`;
+    });
+
+
+
+    var heartIcon = document.getElementById('heartIcon');
+    var scrollDown = document.getElementById('scrollTo');
+
+    heartIcon.addEventListener('click', function () {
+        scrollDown.scrollIntoView({ behavior: 'smooth' });
+    });
+
 });
